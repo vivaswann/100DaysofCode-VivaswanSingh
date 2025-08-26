be84a3bf72fc1353bafbd4cfda2c42f83c0e911f
vivaswann <vivaswan573@gmail.com>
2025-08-26 16:24:22 +0530

    Add files via upload
    ## 📅 Day 1 (User Inputs, Operations & Output)
    Q1: Write a program to input two numbers and display their sum.
    Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.

---
+++ day01_q2.c
@@ -0,0 +1,27 @@
+// Program: Basic Arithmetic Operations
+// Problem: Input two numbers and display their sum, difference, product, and division.
+
+#include <stdio.h>
+
+int main() 
+{
+    float firstnumber, secondnumber;
+
+    printf("Enter first number: ");
+    scanf("%f", &firstnumber);
+
+    printf("Enter second number: ");
+    scanf("%f", &secondnumber);
+
+    printf("Sum = %f\n", firstnumber + secondnumber);
+    printf("Difference = %f\n", firstnumber - secondnumber);
+    printf("Product = %f\n", firstnumber * secondnumber);
+
+    if (secondnumber != 0) {
+        printf("Division = %f\n", firstnumber/secondnumber);
+    } else {
+        printf("ERROR");
+    }
+
+    return 0;
+}
