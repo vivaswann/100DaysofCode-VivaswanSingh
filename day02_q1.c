7ab0ff368c4819070bddf8aa942d78462c042341
vivaswann <vivaswan573@gmail.com>
2025-08-26 16:42:06 +0530

    Update day02_q1.c

--- day02_q1.c
+++ day02_q1.c
@@ -1,3 +1,5 @@
+//Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
 #include <stdio.h>
 
 int main() {
@@ -17,3 +19,4 @@ int main() {
 
     return 0;
 }
+
