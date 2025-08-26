1fd225413bba28200e02ac191b55c68e530cbbe1
vivaswann <vivaswan573@gmail.com>
2025-08-26 16:19:44 +0530

    Add files via upload
    ## 📅 Day 1
    - Q1: Program to input two numbers and display their sum.
    - Q2: Program to input two numbers and display their sum, difference, product, and quotient.

---
+++ day01_q1.c
@@ -0,0 +1,22 @@
+// Day 1 - Q1: User Inputs, Operations & Output
+// Problem: Take two integers as input, perform basic operations, and display the results.
+
+#include <stdio.h>
+
+int main() {
+    float a, b, Sum;
+
+    printf("Enter first number: ");
+    scanf("%f", &a);
+
+    printf("Enter second number: ");
+    scanf("%f", &b);
+
+    Sum= a+b;
+    
+    printf("Sum=%f", Sum);
+
+   
+
+    return 0;
+}
