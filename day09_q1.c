dd6cdf8a66ac53317ca2f96ef2fb1471173dca4e
vivaswann <teenw4ites@gmail.com>
2025-08-29 23:24:16 +0530

    day09

---
+++ day09_q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c, d;
+
+    printf("Quadratic equation example: a(x*x) + bx + c ");
+    printf("\nEnter a, b, c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    d = b*b - 4*a*c;
+
+    if(d > 0) {
+        printf("Roots are real and distinct.\n");
+    }
+    else if(d == 0) {
+        printf("Roots are real and equal.\n");
+    }
+    else {
+        printf("Roots are imaginary (complex).\n");
+    }
+
+    return 0;
+}
---
+++ day_9_q2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int S1, S2, S3, S4, S5;
+    int per = 0, choice;
+
+    printf("Enter marks of S1,S2,S3,S4,S5: ");
+    scanf("%d %d %d %d %d", &S1, &S2, &S3, &S4, &S5);
+
+    per = ((S1 + S2 + S3 + S4 + S5) / 5);
+    choice = per/10;
+
+    switch (choice)
+    {
+    case 10:
+    printf("Either you're a genius or have no social life");
+        break;
+
+    case 9:
+        printf("Grade A");
+        break;
+
+    case 8:
+        printf("Grade B");
+        break;
+
+    case 7:
+        printf("Grade C");
+        break;
+
+    case 6:
+        printf("Grade D");
+        break;
+
+    case 5:
+        printf("Grade F");
+        break;    
+
+    default:
+    printf("Out of Range");
+        break;
+    }
+}
