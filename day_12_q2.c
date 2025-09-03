9880eaec3d8ba028493e4da44ccca16fbedd2b31
vivaswann <teenw4ites@gmail.com>
2025-09-03 18:00:22 +0530

    modified

--- day_12_q2.c
+++ day_12_q2.c
@@ -1,3 +1,9 @@
+/*Write a program to calculate electricity bill based on units consumed with these rates:
+First 100 units at ₹5/unit
+Next 100 units at ₹7/unit
+Next 100 units at ₹10/unit
+Above at ₹12/unit*/
+
 #include <stdio.h>
 
 int main() {
