02c58a16c38aa8210993e32c9c967188ec480967
vivaswann <teenw4ites@gmail.com>
2025-09-14 17:42:42 +0530

    day 25

---
+++ day_25_q2.c
@@ -0,0 +1,21 @@
+/*Write a program to print the following pattern:
+
+*****
+****
+***
+**
+*
+
+*/
+
+#include <stdio.h>
+int main() {
+    for (int i =5;i>=1;i--){
+    for (int j=1;j<=i;j++){
+
+    printf("*");
+    }
+printf("\n");
+}
+    return 0;
+}
